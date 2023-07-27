ba78388a757b90336ef4a3e4315d8068d4ccbfd8
Tales Araújo Carvalho <tales.araujocarv@hotmail.com>
2023-07-27 15:48:08 -0300

    app.py

--- app.py
+++ app.py
@@ -22,26 +22,137 @@ server = app.server
 from globals import *
 
 # Pré-layout ================
-card_faturamento = dbc.Card([
-        #dbc.CardImg(src="/assets/money_up.png", top=True, style={'max-width':'58px','max-height':'58px', 'padding-left':'20px', 'padding-top':'20px'}),
-        
-        dbc.CardBody([
-                html.H4("Faturamento", className="card-title"),
-            ]),
+card_total_partidas = dbc.Card(
+    [
+        dbc.CardImg(src="/assets/image_1.png", top=True, style={'max-width':'58px','max-height':'58px', 'padding-left':'20px', 'padding-top':'20px'}),
+        dbc.CardBody(
+            [
+                html.H5("Partidas", className="card-title"),
+                html.H2(n_partidas_totais)
+            ]
+        ),
     ],
-    style={"width": "90%", 'height':'100%'})
+    style={"width": "18rem"},
+)
+
+card_total_gols = dbc.Card(
+    [
+        dbc.CardImg(src="/assets/bola_simbolo_card.png", top=True, style={'max-width':'45px','max-height':'45px', 'padding-left':'20px', 'padding-top':'20px'}),
+        dbc.CardBody(
+            [
+                html.H5("Gols", className="card-title"),
+                html.H2(n_gols_temporada)
+            ]
+        ),
+    ],
+    style={"width": "18rem"},
+)
+
+card_lider_atual = dbc.Card(
+    [
+        dbc.CardImg(src="/assets/Trofeu_1.png", top=True, style={'max-width':'45px','max-height':'45px', 'padding-left':'20px', 'padding-top':'20px'}),
+        dbc.CardBody(
+            [
+                html.H5("1° geral", className="card-title"),
+                html.H2(lider_geral)
+                
+            ]
+        ),
+    ],
+    style={"width": "18rem"},
+)
+
+card_n_rodadas = dbc.Card(
+    [
+        dbc.CardImg(src="/assets/n_rodadas.png", top=True, style={'max-width':'45px','max-height':'45px', 'padding-left':'20px', 'padding-top':'20px'}),
+        dbc.CardBody(
+            [
+                html.H5("N° de rodadas|Liga", className="card-title"),
+                html.H2(n_rodadas_liga)
+            ]
+        ),
+    ],
+    style={"width": "18rem"},
+)
+
+card_n_copas = dbc.Card(
+    [
+        dbc.CardImg(src="/assets/n_rodadas.png", top=True, style={'max-width':'45px','max-height':'45px', 'padding-left':'20px', 'padding-top':'20px'}),
+        dbc.CardBody(
+            [
+                html.H5("N° de rodadas|Copa", className="card-title"),
+                html.H2(n_rodadas_copa)
+            ]
+        ),
+    ],
+    style={"width": "18rem"},
+)
+
+
+columnDefs = [
+    {'field': 'PLAYER', 'width': 120, 'autosize': True},
+    {'field': 'V', 'width': 60},
+    {'field': 'E', 'width': 60},
+    {'field': 'D', 'width': 60},
+    {'field': 'GOL', 'width': 80},
+    {'field': 'ASS', 'width': 80},
+    {'field': 'STG', 'width': 80},
+    {'field': 'AMA', 'width': 80},
+    {'field': 'AZUL', 'width': 80},
+    {'field': 'VER', 'width': 80},
+    {'field': 'FALTA', 'width': 90},
+    {'field': 'PTS', 'width': 90},
+]
 
 
 # Layout    =================
 app.layout = html.Div([
-    
+    dbc.Row([
+        dbc.Col([
+            dbc.Row([
+                dbc.Col(dbc.Card(card_total_partidas, color="primary", style={"opacity": 0.9}), lg=2),
+                dbc.Col(dbc.Card(card_total_gols, color='secondary', style={"opacity": 0.9}), lg=2),
+                dbc.Col(dbc.Card(card_lider_atual, color='success', style={"opacity": 0.9}), lg=2),
+                dbc.Col(dbc.Card(card_n_rodadas, color='warning', style={"opacity": 0.9}), lg=2),
+                dbc.Col(dbc.Card(card_n_copas, color='#64A9DE', style={"opacity": 0.9}), lg=2),
+                ], style={'padding-bottom':'20px'}), 
+                 
+            dbc.Row(dbc.Card(dbc.CardBody([
+                dcc.Dropdown(id='dpd-01-rg/c1/r2', 
+                            options=[
+                                    {'label': 'Ranking Geral', 'value': 'ranking'},
+                                    {'label': 'TOP Gols', 'value': 'gol'},
+                                    {'label': 'TOP Assistências', 'value': 'assistencia'},
+                                    {'label': 'Goleiros', 'value': 'gks'}], value='ranking'),
+                dcc.Graph(id='grafico-01-rg/c2/r2', style={'height':'400px'}),
+                ]))), 
+            dbc.Row(dag.AgGrid(id="ranking-table", rowData=df_table.to_dict("records"), columnDefs=columnDefs, defaultColDef={"resizable": True, "sortable": True}, style={'height':'300px'})), 
+                ], lg=6),
+        
+        dbc.Col([dbc.Row(), dbc.Row()], lg=6)
+    ])
 ])
 
 
-
 # Callbacks =================
-
-
+@app.callback(
+    Output('grafico-01-rg/c2/r2', 'figure'),
+    [Input('dpd-01-rg/c1/r2', 'value')]
+)
+def update_grafico_um(criterio):
+    if criterio == 'ranking':
+        fig_corrida_geral = px.bar(df_corrida_geral, x='PLAYER', y='PTS', title='PONTUAÇÃO GERAL', text_auto='.2s')
+        return fig_corrida_geral
+    elif criterio == 'gol':
+        fig_top_cinco_artilheiros = px.bar(df_top_cinco_artilheiros, x='PLAYER', y='GOL', title='TOP 5 - ARTILHEIRO')
+        return fig_top_cinco_artilheiros
+    elif criterio == 'assistencia':
+        fig_top_cinco_ass = px.bar(df_top_cinco_assistencia, x='PLAYER', y='ASS', title='TOP 5 - ASSISTS')
+        return fig_top_cinco_ass
+    else:
+        fig_gks = px.bar(df_goleiros_gs, x='PLAYER', y=['GS','STG', 'DD'], barmode="group", title='GKs - GS / STG / DDs')
+        return fig_gks
+        
 # Servidor  =================
 if __name__=='__main__':
     app.run_server(debug=True)
--- globals.py
+++ globals.py
@@ -1,2 +1,59 @@
 import pandas as pd
 
+df = pd.read_excel('data/testedatasetv2b.xlsx')
+df_copas = pd.read_excel('data/copas_mr.xlsx')
+df_season = pd.concat([df, df_copas])
+
+df.fillna(0, inplace=True)
+df_copas.fillna(0, inplace=True)
+df_season.fillna(0, inplace=True)
+
+df_corrida_geral = df_season.groupby('PLAYER').agg({'PTS':'sum', 'GOL':'sum', 'ASS':'sum'})
+df_corrida_geral = df_corrida_geral.sort_values(by='PTS', ascending=False)
+df_corrida_geral = df_corrida_geral.reset_index()
+lider_geral = df_corrida_geral.iloc[0]['PLAYER']
+
+#table
+df_table = df_season.groupby('PLAYER').agg({'V':'sum', 'E':'sum', 'D':'sum', 'GOL':'sum', 'ASS':'sum', 'STG':'sum','AMA':'sum', 'AZUL':'sum', 'VER':'sum','FALTA':'sum', 'PTS':'sum'})
+df_table = df_table.sort_values(by='PTS', ascending=False)
+df_table = df_table.reset_index()
+
+#algumas variáveis
+lista_partidas = df['PARTIDA'].to_list()
+lista_partidas_copas = df_copas['PARTIDA'].to_list()
+n_partidas_totais = lista_partidas[-1] + lista_partidas_copas[-1]
+n_rodadas_liga = df['RODADA'].unique()[-1]
+n_rodadas_copa = df_copas['RODADA'].unique()[-1]
+
+df_top_cinco_artilheiros = df_corrida_geral.sort_values(by='GOL', ascending=False)  
+df_top_cinco_artilheiros = df_top_cinco_artilheiros.reset_index()
+df_top_cinco_artilheiros = df_top_cinco_artilheiros.drop('index', axis=1)
+df_top_cinco_artilheiros = df_top_cinco_artilheiros.iloc[0:5]
+
+df_top_cinco_assistencia = df_corrida_geral.sort_values(by='ASS', ascending=False)  
+df_top_cinco_assistencia = df_top_cinco_assistencia.reset_index()
+df_top_cinco_assistencia = df_top_cinco_assistencia.drop('index', axis=1)
+df_top_cinco_assistencia = df_top_cinco_assistencia.iloc[0:5]
+
+n_gols_temporada = df_season['GOL'].sum()
+
+df_gk = df_season.loc[df_season['POSIÇÃO']=='GK']
+df_gk = df_gk.loc[df_gk['PLAYER']!='GK sub']
+df_gk = df_gk.loc[df_gk['PLAYER']!='Carlos GK']
+df_gk = df_gk.loc[df_gk['PLAYER']!='Victor GK']
+df_goleiros_gs = df_gk.groupby('PLAYER').agg({'STG':'mean','GS':'mean','DD':'mean'})
+df_goleiros_gs = df_goleiros_gs.sort_values(by='GS', ascending=True)
+df_goleiros_gs = df_goleiros_gs.reset_index()
+
+df_liga = pd.read_excel('data/testedatasetv2b.xlsx')
+df_copa = pd.read_excel('data/copas_mr.xlsx')
+df_season = pd.concat([df_liga, df_copa])
+
+df_liga.fillna(0, inplace=True)
+df_copa.fillna(0, inplace=True)  
+
+competicoes = df_season['COMPETIÇÃO'].unique()
+lista_rodadas_liga = df_liga['RODADA'].unique()
+lista_rodadas_copa = df_copa['RODADA'].unique()
+
+lista_criterio = ['STG', 'GC', 'AMA', 'AZUL', 'VER', 'PP', 'FALTA']
